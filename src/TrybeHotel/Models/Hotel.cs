c7d365faeb46cb6d400b2a2f2a5511fd80439e24
Daniel Yabu <danielyabu@yahoo.com>
2025-12-31 15:30:32 -0300

    Feat: implements and tests models + fixes spelling mistake on readme + configures context

--- src/TrybeHotel/Models/Hotel.cs
+++ src/TrybeHotel/Models/Hotel.cs
@@ -4,5 +4,16 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 // 1. Implemente as models da aplicação
 public class Hotel {
+    [Key]
+    public int HotelId { get; set; }
 
+    public string? Name { get; set; }
+    public string? Address { get; set; }
+
+    public int CityId { get; set; }
+
+    [ForeignKey("CityId")]
+    public City? City { get; set; }
+
+    public ICollection<Room>? Rooms { get; set; }
 }
--- src/TrybeHotel/Repository/TrybeHotelContext.cs
+++ src/TrybeHotel/Repository/TrybeHotelContext.cs
@@ -4,6 +4,32 @@ using TrybeHotel.Models;
 namespace TrybeHotel.Repository;
 public class TrybeHotelContext : DbContext, ITrybeHotelContext
 {
+    public DbSet<Hotel> Hotels { get; set; }
+
+    public DbSet<Room> Rooms { get; set; }
+
+    public DbSet<City> Cities { get; set; }
+
     public TrybeHotelContext(DbContextOptions<TrybeHotelContext> options) : base(options) { }
     public TrybeHotelContext() { }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(@"Server=127.0.0.1;Database=master;User=SA;Password=password123!;");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Definição da relação com Author
+        modelBuilder.Entity<Room>()
+            .HasOne(c => c.Hotel)
+            .WithMany(h => h.Rooms)
+            .HasForeignKey(c => c.HotelId);
+
+        // Definição da relação com Publisher
+        modelBuilder.Entity<Hotel>()
+            .HasOne(h => h.City)
+            .WithMany(c => c.Hotels)
+            .HasForeignKey(h => h.CityId);
+    }
 }
